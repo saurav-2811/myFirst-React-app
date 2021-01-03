fb90f92387d90a2814f34a4c78b49b9081bbb5f0
saurav2811 <saurav.sri2811@gmail.com>
2021-01-03 12:27:31 +0530

    resolve href

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import Web from "./web.png"
 function Header(){
     return (
         <div className="footercont">
-        <div className="fcont" id="fcont1"><a className="link" href=""><img className="icon" src= {Phone} alt=""/><p> Toll free <b>1800 200 1234</b></p></a></div>
-        <div className="fcont" id="fcont2"><a className="link" href=""><img className="icon" src={Fb} alt=""/><p>  www.facebook.com/cripumps</p></a></div>
-        <div className="fcont" id="fcont3"><a className="link" href=""><img className="icon" src={Web} alt=""/><p>  ww.crigroups.com</p></a></div>
+        <div className="fcont" id="fcont1"><a className="link" href="/"><img className="icon" src= {Phone} alt=""/><p> Toll free <b>1800 200 1234</b></p></a></div>
+        <div className="fcont" id="fcont2"><a className="link" href="/"><img className="icon" src={Fb} alt=""/><p>  www.facebook.com/cripumps</p></a></div>
+        <div className="fcont" id="fcont3"><a className="link" href="/"><img className="icon" src={Web} alt=""/><p>  ww.crigroups.com</p></a></div>
              </div>
         );
 }
