070c1f1e8b22e172108e9a77df686d7a0571c256
saurav2811 <saurav.sri2811@gmail.com>
2021-01-03 12:31:07 +0530

    resolve

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import logo from "./logo.png"
 function Header(){
     return (
-    <div className="logo" id="logo"><a id="logo-img" href=""><img id="logo-img" src= {logo} alt=""/></a></div>
+    <div className="logo" id="logo"><a id="logo-img" href="/"><img id="logo-img" src= {logo} alt=""/></a></div>
     
         );
 }
